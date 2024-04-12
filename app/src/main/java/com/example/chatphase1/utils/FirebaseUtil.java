1b6b61b1b90cfdde13d0b6b8c9a600edc2b778a5
TrinhMinhTam <126665092+TrinhMinhTam@users.noreply.github.com>
2024-04-12 10:02:49 +0700

    List User in RecyclerView || Done

--- app/src/main/java/com/example/chatphase1/utils/FirebaseUtil.java
+++ app/src/main/java/com/example/chatphase1/utils/FirebaseUtil.java
@@ -1,6 +1,7 @@
 package com.example.chatphase1.utils;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
@@ -16,5 +17,9 @@ public class FirebaseUtil {
     }
     public static DocumentReference currentUserDetails(){
         return FirebaseFirestore.getInstance().collection("users").document(currentUserId());
+    }
+
+    public static CollectionReference allUserCollectionReference(){
+        return FirebaseFirestore.getInstance().collection("users");
     }
 }
